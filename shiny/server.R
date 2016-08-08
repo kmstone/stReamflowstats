853567a77a31fc256ed029b55fc01875b6cd88db
kmstone <katstone@ucdavis.edu>
2016-08-08 13:45:14 -0700

    8/8 update

---
+++ shiny/server.R
@@ -0,0 +1,157 @@
+# TODO: Add comment
+# 
+# Author: kmstone5
+###############################################################################
+
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(dplyr)
+library(curl) # make the jsonlite suggested dependency explicit
+library(ggplot2)
+setwd("C:\\Users\\kmstone5\\git\\stReamflowstats\\shiny")
+
+
+
+gauges <- read.csv("gauges.csv")
+row.names(gauges)<- paste(gauges$site_no)
+
+function(input, output, session) {
+	
+# Map under the "Metrics" tab	
+	output$busmap <- renderLeaflet({
+				leaflet(gauges) %>%
+						addTiles(
+								urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+								attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+						) %>%
+						setView(lng = -120.42, lat = 37.52, zoom = 6)%>%
+#						addMarkers(~long,~lat,popup=~as.character(site_no))
+						addCircleMarkers(~long,~lat,
+								radius = 5,
+								color = "blue",
+								stroke = FALSE, fillOpacity = 0.5,
+								layerId = ~site_no
+						#popup = ~paste("USGS ",as.character(site_no))
+						)
+			})
+	
+# Labeling "Metrics" map with gauge number	
+	showGaugePopup <- function(event) {
+#		selectedGauge <- gauges$site_no[id]
+		content <- as.character(tagList(
+						tags$h4(paste("USGS ",event$id))
+				))
+		leafletProxy("busmap") %>% addPopups(lng=event$lng, lat=event$lat, popup=content)
+	}
+	
+#	# When "Metrics" map is clicked, show a popup of plot below map
+	observe({
+				leafletProxy("busmap", data=gauges) %>% clearPopups()
+				event <- input$busmap_marker_click
+				if (is.null(event))
+					return()
+				
+				isolate({
+							##					leafletProxy("busmap") %>% addPopups(lng=event$lng,
+							##							lat=event$lat, popup=c("blah"))
+							d <- data.frame(x=seq(1,10,1),y=rnorm(10))
+							plotd <- ggplot(d,aes(x,y)) + geom_point() + ggtitle(paste(event$id))
+							#x <- c(1,2,3,4,5)
+							#plotd <- hist(x, breaks = "Sturges", main = paste("Histogram of Specific Gauge"), xlab = "year", ylab = "value")
+							output$plot_d <- renderPlot({
+										plotd
+									})
+							showGaugePopup(event)
+						})
+			})		
+# Map under the "Trends" tab
+#	library(rgdal)	
+#	trendPoint <- readOGR(dsn="C:\\Users\\kmstone5\\Google Drive\\SRA_Kathleen\\trends\\trends_layer packages\\",
+#		layer = "trends_full_dec_feb_vol", verbose = FALSE)
+	output$map_Trends <- renderLeaflet({
+			leaflet(gauges) %>%
+					addTiles(
+							urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+							attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+					) %>%
+					setView(lng = -120.42, lat = 37.52, zoom = 6) 
+#			%>% addMarkers(data = trendPoint)
+		})		
+	
+	
+# full record map under "MAPS" tab
+	output$map_full <- renderLeaflet({
+				leaflet(gauges) %>%
+						addTiles(
+								urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+								attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+						) %>%
+						setView(lng = -120.42, lat = 37.52, zoom = 6)
+			})	
+# post-impairment record map under "MAPS" tab
+	output$map_pi <- renderLeaflet({
+				leaflet(gauges) %>%
+						addTiles(
+								urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+								attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+						) %>%
+						setView(lng = -120.42, lat = 37.52, zoom = 6)
+			})	
+
+# Map under "STARR">"STARR" tab
+	library(rgdal)	
+	states <- readOGR(dsn="C:\\Users\\kmstone5\\Google Drive\\SRA_Kathleen\\shiny_stuff\\Thesis_maps",
+				layer = "CV_huc", verbose = FALSE)
+	output$map_STARR <- renderLeaflet({
+				leaflet(gauges) %>%
+						addTiles(
+								urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+								attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+						) %>%
+						setView(lng = -120.42, lat = 37.52, zoom = 6) %>%
+						addPolygons(data = states,
+								stroke = FALSE, fillOpacity = 0.5, smoothFactor = 0.5)
+			})		
+# DRAFT! Labeling "STARR" map with classifications	
+	showClassPopup <- function(event) {
+		content <- as.character(tagList(
+						tags$h4(paste("USGS ",event$id))
+				))
+		leafletProxy("map_STARR") %>% addPopups(popup=content)
+	}
+	
+# Map under the "OMR" tab
+	library(rgdal)	
+#	trendPoint <- readOGR(dsn="C:\\Users\\kmstone5\\Google Drive\\SRA_Kathleen\\trends\\trends_layer packages\\",
+#		layer = "trends_full_dec_feb_vol", verbose = FALSE)
+	output$map_OMR <- renderLeaflet({
+			leaflet(gauges) %>%
+					addTiles(
+							urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+							attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+					) %>%
+					setView(lng = -120.42, lat = 37.52, zoom = 6) 
+#			%>% addMarkers(data = trendPoint)
+		})		
+
+# Maps under the "Allocation & Demand" tab
+	output$map_ad1 <- renderLeaflet({
+			leaflet(gauges) %>%
+					addTiles(
+							urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+							attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+					) %>%
+					setView(lng = -120.42, lat = 37.52, zoom = 6)
+		})	
+
+	output$map_ad2 <- renderLeaflet({
+			leaflet(gauges) %>%
+					addTiles(
+							urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+							attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+					) %>%
+					setView(lng = -120.42, lat = 37.52, zoom = 6)
+		})	
+
+}
---
+++ shiny/ui.R
@@ -0,0 +1,163 @@
+# TODO: Add comment
+# 
+# Author: kmstone5
+###############################################################################
+
+library(shinydashboard)
+library(leaflet)
+
+shinyUI(navbarPage("Water Available for Ag-GB",
+				
+				tabPanel("Metrics",
+						
+						fluidRow(
+								column(width = 8,
+										box(width = NULL, solidHeader = TRUE,
+												leafletOutput("busmap", height = 600)
+										),
+										box(width = NULL,
+												uiOutput("numVehiclesTable")
+										)
+								),
+								
+								sidebarPanel(width = 4, status = "warning",
+										selectInput("metric1", "Select metric",
+												choices = c("Magnitude" = 1, "Duration" = 2, "Intra-annual frequency" = 3,
+														"Timing" = 4, "Inter-annual frequency" = 5, "Average total flow" = 6),
+												selected = "1"),
+										uiOutput("metric1choice"),
+										
+										selectInput("metric2", "Select time period",
+												choices = c("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+														"June" = 6, "July" = 7, "August" = 8, "September" = 9,
+														"October" = 10, "November" = 11, "December" = 12, "3-month" = 13,
+														"6-month" = 14, "Hydrologic year" = 15),
+												selected = "1"),
+										uiOutput("metric2choice"),
+										
+										radioButtons("metric3", "Select record length",
+												c("Full record" = 1,
+														"Post-impairment record" = 2), selected = 1),
+										
+										radioButtons("metric4", "Select water year type",
+												c("Average" = 1,
+														"All water year types" = 2), selected = 1),
+										
+										actionButton("selection", "Submit"),
+										p(class = "text-muted",
+												br(),
+												"Source data updates every 30 seconds."
+										)
+								),
+								mainPanel(width = 12,
+										box(width = NULL, status = "warning",
+												plotOutput("plot_d")
+										)
+								)
+						)),
+				tabPanel("Trends",
+						fluidRow(
+								column(width=8,
+										box(leafletOutput("map_Trends", height = 700), width = NULL, solidHeader = TRUE)
+								),
+								sidebarPanel(selectInput("timeperiod2", "Select time period",
+												choices = c("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+														"June" = 6, "July" = 7, "August" = 8, "September" = 9,
+														"October" = 10, "November" = 11, "December" = 12, "3-month" = 13,
+														"6-month" = 14, "Hydrologic year" = 15),
+												selected = "1"),
+										uiOutput("timeperiodchoice2"), width = 4,
+										actionButton("selection2", "Submit")))),
+				tabPanel("Maps",
+						fluidRow(
+								column(width=6, h2("Full Record Length"),
+										box(leafletOutput("map_full", height = 700), width = NULL, solidHeader = TRUE)
+								),
+								column(width=6, h2("Post-Impairment Record Length"),
+										box(leafletOutput("map_pi", height = 700), width = NULL, solidHeader = TRUE)
+								),
+								absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+										draggable = TRUE, top = 300, left = "auto", right = 20, bottom = "auto",
+										width = "auto", height = "auto",
+										h4("Map parameter selection"),	
+										selectInput("metric", "Select metric",
+												choices = c("Magnitude" = 1, "Duration" = 2, "Intra-annual frequency" = 3,
+															"Timing" = 4, "Inter-annual frequency" = 5),
+												selected = "1"),
+										uiOutput("metricchoice"),
+										
+										selectInput("timeperiod", "Select time period",
+												choices = c("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+														"June" = 6, "July" = 7, "August" = 8, "September" = 9,
+														"October" = 10, "November" = 11, "December" = 12, "3-month" = 13,
+														"6-month" = 14, "Hydrologic year" = 15),
+												selected = "1"),
+										uiOutput("timeperiodchoice"),
+										
+										radioButtons("wateryear", "Select water year type",
+												c("Average" = 1, "All water year types" = 2), selected = 1),
+										uiOutput("wateryearchoice"),
+										
+										actionButton("selection", "Submit"),
+										p(class = "text-muted",
+												br(),
+												"Source data updates every 30 seconds."
+										)
+								)
+								
+						)),
+				
+				navbarMenu("STARR",
+						tabPanel("STARR",
+							fluidRow(
+									column(width=8,
+											box(leafletOutput("map_STARR", height = 700), width = NULL, solidHeader = TRUE)
+									),
+									sidebarPanel(selectInput("timeperiod1", "Select time period",
+											choices = c("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+														"June" = 6, "July" = 7, "August" = 8, "September" = 9,
+														"October" = 10, "November" = 11, "December" = 12, "3-month" = 13,
+														"6-month" = 14, "Hydrologic year" = 15),
+											selected = "1"),
+											uiOutput("timeperiodchoice1"), width = 4,
+									actionButton("selection1", "Submit")))),
+					
+						tabPanel("OMR",
+								fluidRow(
+										column(width=8,
+												box(leafletOutput("map_OMR", height = 700), width = NULL, solidHeader = TRUE)
+										),
+										sidebarPanel(selectInput("timeperiod3", "Select time period",
+														choices = c("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+																"June" = 6, "July" = 7, "August" = 8, "September" = 9,
+																"October" = 10, "November" = 11, "December" = 12, "3-month" = 13,
+																"6-month" = 14, "Hydrologic year" = 15),
+														selected = "1"),
+												uiOutput("timeperiodchoice3"), width = 4,
+												actionButton("selection3", "Submit"))))),
+				tabPanel("Allocation & Demand",
+						fluidRow(
+								column(width=6,
+										box(leafletOutput("map_ad1", height = 700), width = NULL, solidHeader = TRUE)
+								),
+								column(width=6,
+										box(leafletOutput("map_ad2", height = 700), width = NULL, solidHeader = TRUE)
+								),
+								absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+										draggable = TRUE, top = 300, left = "auto", right = 20, bottom = "auto",
+										width = "auto", height = "auto",
+										h4("Map parameter selection"),	
+										selectInput("timeperiod3", "Select time period",
+												choices = c("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+														"June" = 6, "July" = 7, "August" = 8, "September" = 9,
+														"October" = 10, "November" = 11, "December" = 12, "3-month" = 13,
+														"6-month" = 14, "Hydrologic year" = 15),
+												selected = "1"),
+										actionButton("selection", "Submit")
+										)
+								)
+						
+						),
+				tabPanel("About"),
+				id = "nav", theme = "bootstrap.css"))
+
